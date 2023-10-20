ff560697a7692418db9f04ffdb4609b8b5b3831b
ze-fernando <jfenginner935@gmail.com>
2023-10-19 22:11:20 -0300

    Create calculator

---
+++ Models/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace DIO_project;
+
+public class Calculator{
+    public static int soma(int x, int y){
+        return x + y;
+    }
+    public static int subtracao(int x, int y){
+        return x - y;
+    }
+    public static int divisao(int x, int y){
+        return x / y;
+    }
+    public static int multiplicacao(int x, int y){
+        return x * y;
+    }
+}
